d42372ad847737c54223588072b162d4c1c8890c
Adolam <sigma_adolam@hotmail.com>
2024-08-25 01:04:21 -0600

    se realizaron las validaciones del formulario

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -11,8 +11,13 @@ class RegisterController extends Controller
         return view('auth.register');
     }
 
-    public function store()
+    public function store(Request $request)
     {
-        dd('Post...');
+        $request->validate([
+            'name' => 'required|max:30',
+            'username'=> 'required|unique:users|min:3|max:20',
+            'email' => 'required|unique:users|email|max:60',
+            'password' => 'required'
+        ]);
     }
 }

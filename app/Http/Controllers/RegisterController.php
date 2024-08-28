2f9c234441de087e6d52b3bbcdb4d6fd833875db
adolam <sigma_adolam@hotmail.com>
2024-08-28 11:22:06 -0600

    agregando demás validaciones a formulario y respuestas post

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -13,11 +15,25 @@ class RegisterController extends Controller
 
     public function store(Request $request)
     {
+
+        //Modificando el request
+        $request->request->add(['username'=> Str::slug($request->username)]);
+
         $request->validate([
             'name' => 'required|max:30',
             'username'=> 'required|unique:users|min:3|max:20',
             'email' => 'required|unique:users|email|max:60',
-            'password' => 'required'
+            'password' => 'required|confirmed|min:6'
         ]);
+
+        User::create([
+            'name'=>$request->name,
+            'username'=>$request->username,
+            'email'=>$request->email,
+            'password'=>$request->password
+        ]);
+
+        //Redireccionando al muro
+        return redirect()->route('post.index');
     }
 }
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -22,13 +22,12 @@
                         type="text"
                         placeholder="Tu nombre"
                         class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror"
-                        value="{{ old('name')}}" />
+                        value="{{ old('name')}}">
                     @error('name')
                         <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
                             {{ $message }}
                         </p>
                     @enderror
-
                 </div>
                 <div class="mb-5">
                     <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
@@ -40,7 +39,7 @@
                         type="text"
                         placeholder="Tu nombre de usuario"
                         class="border p-3 w-full rounded-lg @error('username') border-red-500 @enderror"
-                        value="{{ old('username')}}/>
+                        value="{{ old('username')}}"">
                     @error('username')
                         <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
                             {{ $message }}
@@ -57,7 +56,7 @@
                         type="email"
                         placeholder="Tu Email de Registro"
                         class="border p-3 w-full rounded-lg @error('email') border-red-500 @enderror"
-                        value="{{ old('email')}}/>
+                        value="{{ old('email')}}"/>
                     @error('email')
                         <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
                             {{ $message }}
@@ -73,8 +72,7 @@
                         name="password"
                         type="password"
                         placeholder="Password de Registro"
-                        class="border p-3 w-full rounded-lg @error('password') border-red-500 @enderror"
-                        value="{{ old('password')}}/>
+                        class="border p-3 w-full rounded-lg @error('password') border-red-500 @enderror"/>
                     @error('password')
                         <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
                             {{ $message }}

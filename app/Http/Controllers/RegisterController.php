21009910fec3ebc8a9d87ff4d15ca9c8c0d8a865
Adolam <sigma_adolam@hotmail.com>
2024-08-28 22:45:56 -0600

    Se trabajó con la autenticación y el muro

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class RegisterController extends Controller
 {
@@ -32,6 +33,20 @@ class RegisterController extends Controller
             'email'=>$request->email,
             'password'=>$request->password
         ]);
+
+        //Autenticar al usuario
+        //auth()->attempt([
+        //    'email'=>$request->email,
+        //    'password'=>$request->password
+        //]);
+
+        //Otra forma de autenticar que no probré si funciona
+        //Auth::attempt($request->only('email', 'password'));
+
+        //Otra forma de autenticar
+
+        auth()->attempt($request->only('email', 'password'));
+
 
         //Redireccionando al muro
         return redirect()->route('post.index');

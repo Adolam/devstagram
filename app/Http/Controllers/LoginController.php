1fa5dc61a97f6c70fd055403f0affd3bec07e91c
Adolam <sigma_adolam@hotmail.com>
2024-09-01 04:16:20 -0600

    Se agregaron comentarios

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -19,11 +19,14 @@ class LoginController extends Controller
             'password' => 'required'
         ]);
 
+        //Si el usuario no está logeado, verificará que las credenciales de acceso
+        //son correctas
         if(!auth()->attempt($request->only('email', 'password'), $request->remember))
         {
             return back()->with('mensaje', 'Credenciales Incorrectas');
         }
 
+        //Si es usuario se logea correctamente será redirecconado al muro o feed
         return redirect()->route('post.index');
     }
 

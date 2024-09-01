365178c2dd1ec797a8804112107481ea97c14ee6
adolam <sigma_adolam@hotmail.com>
2024-09-01 03:55:10 -0600

    Se iniciaron los ajustes para el cierre de sesión

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -7,4 +7,24 @@ use Illuminate\Http\Request;
 class LoginController extends Controller
 {
     //
+    public function index ()
+    {
+        return view('auth.login');
+    }
+
+    public function store(Request $request)
+    {   
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if(!auth()->attempt($request->only('email', 'password'), $request->remember))
+        {
+            return back()->with('mensaje', 'Credenciales Incorrectas');
+        }
+
+        return redirect()->route('post.index');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
@@ -10,5 +12,9 @@ Route::get('/', function () {
 
 Route::get('/register', [RegisterController::class, 'index'])->name('register');
 Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/login', [LoginController::class, 'index'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+Route::post('/logout', [LogoutController::class, 'index'])->name('logout');
 
 Route::get('/muro', [PostController::class, 'index'])->name('post.index')->middleware('auth');

d29df6bc8282b3e1931af96fb23b3e6c6db82246
adolam <sigma_adolam@hotmail.com>
2024-09-13 13:24:53 -0600

    Se termina la integración de dropzone y se instala intervention/image-laravel

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,3 +10,21 @@ const dropzone = new Dropzone('#dropzone', {
     maxFiles: 1,
     uploadMultiple: false,
 });
+
+dropzone.on('sending', function(file, xhr, formData) {
+    console.log('Enviando archivo');
+});
+
+dropzone.on('success', function(file, response) {
+    console.log(response);
+});
+
+dropzone.on('error', function(file, message) {
+    console.log(message);
+});
+
+dropzone.on('removedfile', function(file) {
+    console.log('Archivo eliminado');
+});
+
+

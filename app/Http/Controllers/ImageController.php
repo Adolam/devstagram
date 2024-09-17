81afa2a269481e88263ddcd96764ceccc70efdda
adolam <sigma_adolam@hotmail.com>
2024-09-17 11:28:56 -0600

    Se agrega termina la capacidad de almacenar imágenes

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,15 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class ImageController extends Controller
 {
-    //
     public function store(Request $request)
     {
         $imagen = $request->file('file');
 
-        return response()->json(['imagen'=> $imagen->extension() ]);
+        $nombreImagen = Str::uuid() . "." . $imagen->extension();
+
+        $manager = new ImageManager(new Driver());
+        $imagenServidor = $manager->read($imagen);
+        $imagenServidor->cover(1000, 1000);
+
+        $imagenPath = public_path('uploads') . '/' . $nombreImagen;
+        $imagenServidor->save($imagenPath);
+
+        return response()->json(['imagen'=> $nombreImagen ]);
     }
 }

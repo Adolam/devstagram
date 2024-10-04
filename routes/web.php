f56c3e1eebaa610fbb8ddf34fa018e2dc06d41a5
adolam <sigma_adolam@hotmail.com>
2024-10-04 15:04:55 -0600

    Se continúa mejorando  la vista show.post

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
 Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
-Route::get('/{user:username}', [PostController::class, 'index'])->name('post.index')->middleware('auth');
+Route::get('/{user:username}', [PostController::class, 'index'])->name('post.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('post.create')->middleware('auth');
 Route::post('/posts', [PostController::class, 'store'])->name('post.store');
 Route::get('{user:username}/posts/{post}', [PostController::class, 'show'])->name('post.show');

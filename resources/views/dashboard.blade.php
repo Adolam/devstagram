547d69c664b3c9677a957a9fa268d28a4c3c1498
Adolam <sigma_adolam@hotmail.com>
2024-10-24 21:03:38 -0600

    se crea un componente

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -73,25 +73,7 @@
 
     <section class="container mx-auto mt-10">
         <h2 class="text-4xl font-black text-center my-10">Publicaciones</h2>
-        
-
-        @if ($posts->count() )
-            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                @foreach ($posts as $post)
-                    <div class="">
-                        <a href="{{ route('post.show', ['user'=>$user, 'post'=>$post]) }}">
-                            <img src="{{ asset('uploads/' . $post->imagen) }}" alt="Imagen del post {{ $post->titulo }}">
-                        </a>
-                    </div>
-                @endforeach
-            </div>
-
-            <div class="my-10">
-                {{ $posts->links() }}
-            </div>
-        @else
-            <p class="text-gray-600 uppercase text-sm text-center font-bold">No hay posts</p>
-        @endif
+        <x-listar-post :posts="$posts" />
     </section>
 
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,17 +5,5 @@ Portada
 @endsection
 
 @section('contenido')
-    @if ($posts->count())
-        <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-            @foreach ($posts as $post)
-                <div>
-                    <a href="{{ route('post.show', ['post' => $post, 'user' => $post->user]) }}">
-                        <img src="{{ asset('uploads') . '/' . $post->imagen }}" alt="Imagen del post {{ $post->titulo }}">
-                    </a>
-                </div>
-            @endforeach
-        </div>
-    @else
-        <p class="text-center">No hay posts, sigue a alguien para ver sus posts</p>
-    @endif
+    <x-listar-post :posts="$posts"/>
 @endsection
